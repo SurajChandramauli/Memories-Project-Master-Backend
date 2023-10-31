787aace36970330e1f4ae3868fd409b1a6e8bb37
Suraj Chandramauli <59973731+SurajChandramauli@users.noreply.github.com>
2023-10-31 18:51:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,20 +8,27 @@ import dotenv from "dotenv";
 var app = express();
 
 dotenv.config();
+const PORT = process.env.PORT || 5000
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.CONNECTION_URL)
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+connectDB().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+})
 
 // mongoose.set("useFindAndModify", false);
